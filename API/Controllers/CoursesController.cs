493afefb3bb99506c4aa2e8317ae5857818db6ec
Hauxii <haukur91@gmail.com>
2016-09-28 15:42:53 +0000

    added coursescontroller

---
+++ API/Controllers/CoursesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using CoursesAPI.Models;
+using CoursesAPI.Services.DataAccess;
+using CoursesAPI.Services.Services;
+
+namespace CoursesAPI.Controllers
+{
+	[Route("api/courses")]
+	public class CoursesController : Controller
+	{
+		private readonly CoursesServiceProvider _service;
+
+		public CoursesController(IUnitOfWork uow)
+		{
+			_service = new CoursesServiceProvider(uow);
+		}
+
+		[HttpGet]
+		public IActionResult GetCoursesBySemester(string semester = null)
+		{
+			// TODO: figure out the requested language (if any!)
+			// and pass it to the service provider!
+			return Ok(_service.GetCourseInstancesBySemester(semester));
+		}
+
+		/// <summary>
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="model"></param>
+		/// <returns></returns>
+		[HttpPost]
+		[Route("{id}/teachers")]
+		public IActionResult AddTeacher(int id, AddTeacherViewModel model)
+		{
+			var result = _service.AddTeacherToCourse(id, model);
+			return Created("TODO", result);
+		}
+	}
+}
